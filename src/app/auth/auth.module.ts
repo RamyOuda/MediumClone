7bb0050265cada07d4d7538e0ba4df36122fbe94
Ramy Ouda <ramyouda@gmail.com>
2023-06-15 18:26:49 -0400

    import auth module and setup register route

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './components/register/register.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [{ path: 'register', component: RegisterComponent }];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
