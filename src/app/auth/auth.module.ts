a240f10ae0e4d878e59a4b96721491242d85c02d
Ramy Ouda <ramyouda@gmail.com>
2023-06-16 11:53:33 -0400

    create store and reducer

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { RegisterComponent } from './components/register/register.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/reducers';
 
 const routes = [{ path: 'register', component: RegisterComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+  ],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
